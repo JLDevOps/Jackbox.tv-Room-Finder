0e5fff11dcd1aa5681e31aedcc2af02c1559278a
Jimmy <jldevops@gmail.com>
2018-10-25 00:53:08 -0400

    Updated readme and cleaned up the script to only locate the open rooms

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Jackbox.tv-Room-Finder
-Python script to find all the available Jackbox.tv rooms.
+# Jackbox.TV Room Finder
+
+*Never Play Jackbox Games Alone.*
+
+This script will find all the available rooms for anyone to join.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine. 
+
+### Installations
+
+The following steps go through installing Python dependencies and setting up the environment for the tool.
+
+1. Install the Python modules for this tool
+    ```
+    $ pip install -r requirements.txt
+    ```
+
+### Usage
+
+#### Running the Script
+1. Run the following command
+    ```bash
+    $ python room_finder.py
+    ```
+2. Here is a sample output:
+    ```json
+    {'roomid': 'AALJ', 'apptag': 'bombintern', 'appid': 'CCJhi8No2mKeADbC474Zn81Vstm7WyBP', 'nu
+     mPlayers': 2, 'numAudience': 0, 'joinAs': 'full', 'locked': True}
+    ```
+    
+## Built With
+
+* Python 3.6
+
+## Authors
+
+* **Jimmy Le** - [Jldevops](https://github.com/jldevops)
+
+
+## License
+
+Licensed under the [MIT License](LICENSE)
+
--- room_finder.py
+++ room_finder.py
@@ -1,38 +1,18 @@
 import requests
 from itertools import product
-import time
-from random import randint
-import threading
-import os, sys
 
 base_url = "https://blobcast.jackboxgames.com/room/"
 exitFlag = 0
 
-class roomThread(threading.Thread):
-    def __init__(self, code, room_name):
-        threading.Thread.__init__(self)
-        self.code = code
-        self.room_name = room_name
-        self.kill_received = False
-
-    def run(self):
-        print("Starting " + self.room_name)
-        while not self.kill_received:
-            test_room_code(self.code)
-        print("Exiting " + self.room_name)
-
-
 def test_room_code(code):
-    # seconds = randint(1, 5)
-    seconds = 5
     try:
         jackbox_url = base_url + str(code)
         r = requests.get(url=jackbox_url)
         data = r.json()
-        print(str(data))
+        if r.status_code != 404:
+            print(data)
     except Exception as e:
         print("Error: " + str(e))
-    time.sleep(seconds)
 
 
 def generate_room_codes(length):
@@ -42,24 +22,9 @@ def generate_room_codes(length):
 
 
 def main():
-    threads = []
     code_list = generate_room_codes(4)
-    # for code in code_list[:10]:
-    #     rm_thread = roomThread(code, str(code))
-    #     threads.append(rm_thread)
-    #     rm_thread.start()
-    #
-    # while len(threads) > 0:
-    #     try:
-    #         threads = [t.join(1) for t in threads if t is not None and t.isAlive()]
-    #     except KeyboardInterrupt:
-    #         for t in threads:
-    #             t.kill_received = True
-
     for code in code_list:
-        seconds = randint(1, 20)
         test_room_code(code)
-        time.sleep(seconds)
 
 
 if __name__ == "__main__":
