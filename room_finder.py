6c6a8da27a5d0b89c17ac488495036a25bfee2ac
Jimmy <jldevops@gmail.com>
2018-11-03 01:19:24 -0400

    Updated the room_finder script to run parallel to shorten the time of retrieval.  Also added a function to check if one of the rooms is available or not.  Will retrieve all the data from the room

--- room_finder.py
+++ room_finder.py
@@ -1,33 +1,23 @@
-import requests
-import json
-from itertools import product
-from concurrent.futures import ThreadPoolExecutor, as_completed, thread
-import time
-import threading
 import argparse
 import csv
-import signal
-from multiprocessing import Pool
+import datetime
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from itertools import product
+
+import requests
 
 base_url = "https://blobcast.jackboxgames.com/room/"
-exitFlag = 0
 csv_writer_lock = threading.Lock()
 
 
-def initializer():
-    signal.signal(SIGINT, SIG_IGN)
-
-
-pool = Pool(initializer=initializer)
-
-
-def test_room_code(code, csv_file):
+def test_room_code(code, csv_file=None):
     try:
         jackbox_url = base_url + str(code)
         r = requests.get(url=jackbox_url)
         data = r.json()
-        if r.status_code != 404:
-            print(data)
+        if r.status_code == 200:
             try:
                 server = data['server']
             except Exception as e:
@@ -40,12 +30,20 @@ def test_room_code(code, csv_file):
 
             room_row = str(data['roomid']) + ',' + server + ',' + data['apptag'] + ',' + data[
                 'appid'] + ',' + str(data['numPlayers']) + ',' + str(data['numAudience']) + ',' + data[
-                           'joinAs'] + ',' + str(locked) + ','
+                           'joinAs'] + ',' + str(locked) + ',' + str(
+                datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')) + ','
+        else:
+            room_row = str(code) + ',,,,,,,,' + str(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')) + ','
+
+        if csv_file:
             with csv_writer_lock:
                 with open(csv_file, 'a', newline='') as csv_file:
                     csv_writer = csv.writer(csv_file)
                     csv_writer.writerow([str(room_row)])
+        else:
+            print(room_row)
             return room_row
+
     except Exception as e:
         print("Error: " + str(e))
 
@@ -57,7 +55,7 @@ def generate_room_codes(length):
 
 
 def create_csv(filename):
-    header = ['RoomId', 'Server', 'Game', 'AppID', 'NumPlayers', 'NumAudience', 'JoinAs', 'Locked']
+    header = ['RoomId', 'Server', 'Game', 'AppID', 'NumPlayers', 'NumAudience', 'JoinAs', 'Locked', 'Last_Checked']
     with open(filename, 'w') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(header)
@@ -68,14 +66,19 @@ def main():
     st = time.time()
     parser = argparse.ArgumentParser(description='Checks if there are any available room in Jackbox.Tv')
     parser.add_argument('-c', '--csv', help='Output CSV file', required=False)
+    parser.add_argument('-r', '--room', help='Room Code', required=False)
+
     args = vars(parser.parse_args())
     csv_filename = args['csv']
-
+    room_code = args['room']
     code_list = generate_room_codes(4)
 
     if csv_filename:
-        print("Csv created")
         create_csv(csv_filename)
+
+    if room_code:
+        room_status = test_room_code(room_code, csv_filename)
+        print(room_status)
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(test_room_code, code, csv_filename) for code in code_list]
@@ -83,8 +86,9 @@ def main():
             for future in as_completed(futures):
                 future.result()
         except KeyboardInterrupt as e:
-            executor._threads.clear()
-            thread._threads_queues.clear()
+            raise
+            # executor._threads.clear()
+            # thread._threads_queues.clear()
 
     print("total time: %s" % (time.time() - st))
 
